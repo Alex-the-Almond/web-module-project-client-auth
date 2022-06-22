496bac08758d9b95644f051c809d5a7786bdc500
Alexander Aymond <lynxkhaos@gmail.com>
2022-06-22 10:19:52 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+
+const Login = () => {
+  return (<h2>Login</h2>)
+}
+
+const FriendList = () => {
+  return (<h2>FriendsList</h2>)
+}
+
+const AddFriend = () => {
+  return (<h2>AddFriends</h2>)
+}
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <Route exact path='/'>
+        <Login/>
+      </Route>
+      <Route exact path='/login'>
+        <Redirect to='/' />
+      </Route>
+      <Route exact path='/friends'>
+        <FriendList/>
+      </Route>
+      <Route exact path='/friends/add'>
+        <AddFriend/>
+      </Route>
     </div>
+    </Router>
   );
 }
 
